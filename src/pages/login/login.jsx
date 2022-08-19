86496dfb08c28e69251bb1455292a1c7d15e03d5
Rafael Raya <rafa-raya@hotmail.com>
2022-08-18 22:36:54 -0500

    se rediseño el login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,14 +2,17 @@ import './sass/login.scss';
 import eye from '../../assets/icons/eye.png';
 import padlock from '../../assets/icons/padlock.png';
 import user from '../../assets/icons/user.png';
-import logo_pokemon from '../../assets/img/logo_pokeapi.png';
+import pokemon_one from '../../assets/img/pokemon_one.png';
 
 const Login = () => {
 	return (
 		<div className="login_container">
-			<div className="form_login">
-				<img className="img_logo" src={logo_pokemon} alt="logo pokemon" />
+			<div className="container_left">
+				<img className="img_logo" src={pokemon_one} alt="pokemon" />
+			</div>
+			<div className="container_right">
 				<form>
+					<h2>WELCOME!</h2>
 					<div className="container_input">
 						<img src={user} />
 						<input />
